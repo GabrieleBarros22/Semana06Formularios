9ad7f57ab790482b509ec37aa94e08bef82ec808
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-11-03 07:10:29 -0300

    att

--- app.py
+++ app.py
@@ -13,23 +13,25 @@ Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('Informe o seu nome', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    lastname = StringField('Informe o seu sobrenome', validators=[DataRequired()])
     institution = StringField('Informe a sua Instituição de ensino')
     disciplina = SelectField('Informe a sua disciplina:', choices=[('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de Projetos', 'Gestão de Projetos')])
+    submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
+        session['lastname'] = form.lastname.data
         session['institution'] = form.institution.data
         session['disciplina'] = form.disciplina.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), lastname=session.get('lastname'), institution=session.get('institution'), disciplina=session.get('disciplina'), current_time=datetime.utcnow())
 
-@app.route('/user/<name>/<institution>/<disciplina>')
-def user(name, institution, disciplina):
-    return render_template('user.html', name=name, institution=institution, disciplina=disciplina)
+@app.route('/user/<name>/<lastname>/<institution>/<disciplina>')
+def user(name, lastname, institution, disciplina):
+    return render_template('user.html', name=name, lastname=lastname, institution=institution, disciplina=disciplina)
 
 @app.route('/contextorequisicao/<name>')
 def contextorequisicao(name):
