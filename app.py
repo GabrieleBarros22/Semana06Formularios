8f632ed2054efec7e5f5c643665ddc80c5809a08
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-11-03 07:01:53 -0300

    att

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -14,14 +14,29 @@ Moment(app)
 class NameForm(FlaskForm):
     name = StringField('Informe o seu nome', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    institution = StringField('Informe a sua Instituição de ensino')
+    disciplina = SelectField('Informe a sua disciplina:', choices=[('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de Projetos', 'Gestão de Projetos')])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  form = NameForm()
-  if form.validate_on_submit():
-    session['name'] = form.name.data
-    return redirect(url_for('index'))
-  return render_template('index.html', form=form, name=session.get('name'))
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['institution'] = form.institution.data
+        session['disciplina'] = form.disciplina.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+
+@app.route('/user/<name>/<institution>/<disciplina>')
+def user(name, institution, disciplina):
+    return render_template('user.html', name=name, institution=institution, disciplina=disciplina)
+
+@app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    user_agent = request.headers.get('User-Agent')
+    remote_addr = request.remote_addr
+    remote_host = request.host
+    return render_template('contexto.html', name=name, user_agent=user_agent, remote_addr=remote_addr, remote_host=remote_host)
 
 @app.errorhandler(404)
 def page_not_found(e):
